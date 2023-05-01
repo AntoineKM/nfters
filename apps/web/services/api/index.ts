3537c732d62abab77fa55f3727017b2b4741b597
Antoine Kingue <etorefr@gmail.com>
2023-05-01 22:30:50 +0200

    ✏️ update env name from api service

--- apps/web/services/api/index.ts
+++ apps/web/services/api/index.ts
@@ -1,3 +1,4 @@
-export const endpoint = process.env.API_URL || "http://localhost:8000";
+export const endpoint =
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
 
 export const fetcher = (url: string) => fetch(url).then((res) => res.json());
