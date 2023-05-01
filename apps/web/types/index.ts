8af1411cd728a34bbad43dc9418062d7a4a543af
Antoine Kingue <etorefr@gmail.com>
2023-05-01 21:03:56 +0200

    🏷️ update nft types with token id

--- apps/web/types/index.ts
+++ apps/web/types/index.ts
@@ -1,4 +1,4 @@
-export type NFT = { metadata: string; token_id: string };
+export type NFT = { metadata: string; token_id: string; token_address: string };
 
 export type NFTMetadata = {
   name: string;
