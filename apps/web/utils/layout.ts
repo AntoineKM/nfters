613ddc05819769e081834957bf7c5f37ba6241b7
Antoine Kingue <etorefr@gmail.com>
2023-05-01 20:28:39 +0200

    ✨ add ipfs to http converter util

--- apps/web/utils/layout.ts
+++ apps/web/utils/layout.ts
@@ -14,3 +14,9 @@ export const formatAdress = (address: string) => {
   if (address.length < 8) return "";
   return `${address.substr(0, 4)}...${address.substr(address.length - 4, 4)}`;
 };
+
+export const convertIPFStoHTTP = (ipfs: string) => {
+  const imagePath = ipfs.split("://")[1];
+  const url = `https://ipfs.io/ipfs/${imagePath}`;
+  return url;
+};
