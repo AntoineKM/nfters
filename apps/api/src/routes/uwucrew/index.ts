7c118586c304503c26335271948b858a3eec7f68
Antoine Kingue <etorefr@gmail.com>
2023-05-01 20:31:13 +0200

    ✨ add uwucrew route to api

---
+++ apps/api/src/routes/uwucrew/index.ts
@@ -0,0 +1,24 @@
+// routes/uwucrew/index.ts
+import { EvmChain } from "@moralisweb3/common-evm-utils";
+import { Router } from "express";
+import Moralis from "moralis";
+
+const address = "0xf75140376d246d8b1e5b8a48e3f00772468b3c0c";
+const chain = EvmChain.ETHEREUM;
+const uwucrewRouter = Router();
+
+uwucrewRouter.get("/", async (req, res) => {
+  const limit = req.query.limit ? parseInt(req.query.limit as string, 10) : 10;
+  const cursor = req.query.cursor ? (req.query.cursor as string) : undefined;
+
+  const response = await Moralis.EvmApi.nft.getContractNFTs({
+    address,
+    chain,
+    limit,
+    cursor,
+  });
+
+  res.send(response.toJSON());
+});
+
+export default uwucrewRouter;
