f4722c7ddfd37456e366d9f29d585d282521e9e1
Antoine Kingue <etorefr@gmail.com>
2023-05-01 21:29:11 +0200

    ✨ add smart contracts

---
+++ apps/contracts/contracts/Nfters.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract Nfters is ERC721 {
+
+  constructor(string memory name, string memory symbol) 
+    ERC721(name, symbol) {
+  }
+
+}
