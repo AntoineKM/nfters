aaabca1b865753baa82cd9d6c6d402653c7f273e
Antoine Kingue <etorefr@gmail.com>
2023-05-05 19:44:23 +0200

    🎨 improve auctions handlers with bids routes

--- apps/api/src/routes/auctions/index.ts
+++ apps/api/src/routes/auctions/index.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { create } from "ipfs-http-client";
 import multer from "multer";
 
-import Auction from "../../models/Auction";
+import AuctionModel from "../../models/Auction";
+import AuctionBidModel from "../../models/AuctionBid";
 import { host, port, protocol, apiKey, apiSecret } from "../../services/ipfs";
 import Log from "../../utils/log";
 
@@ -33,17 +34,28 @@ const upload = multer({
   },
 });
 
+const isHexAddress = (address: string) => {
+  return /^0x[a-fA-F0-9]{40}$/.test(address);
+};
+
 auctionsRouter.get("/", async (req, res) => {
   const limit = req.query.limit ? parseInt(req.query.limit as string, 10) : 10;
   const cursor = req.query.cursor ? parseInt(req.query.limit as string, 10) : 0;
+  const category = req.query.category ? req.query.category : undefined;
 
   try {
-    const auctions = await Auction.find({}).limit(limit).skip(cursor);
+    const auctions = await AuctionModel.find({
+      ...(category && { category: category as string }),
+    })
+      .limit(limit)
+      .skip(cursor)
+      .populate("bids");
 
     res.send({
       auctions,
       cursor:
-        (await Auction.countDocuments()) <= (cursor === 0 ? 1 : cursor) * limit
+        (await AuctionModel.countDocuments()) <=
+        (cursor === 0 ? 1 : cursor) * limit
           ? undefined
           : cursor + 1,
     });
@@ -63,16 +75,16 @@ auctionsRouter.get("/categories", async (_req, res) => {
 
 auctionsRouter.post("/add", upload.single("image"), async (req, res) => {
   try {
-    const { title, startingPrice = 0, endingPrice, category } = req.body;
+    const { title, startingPrice = 0, endingAt, category, owner } = req.body;
     const file = req.file as Express.Multer.File;
-    const auctionsCount = await Auction.countDocuments();
+    const auctionsCount = await AuctionModel.countDocuments();
     if (auctionsCount >= 48) {
       throw new Error("You can only have 48 auctions at a time");
     }
 
-    if (!title || !endingPrice || !category || !file) {
+    if (!title || !endingAt || !category || !file || !owner) {
       throw new Error(
-        "Please provide a title, ending price, category and image"
+        "Please provide a title, ending date, category, image and owner"
       );
     }
 
@@ -82,25 +94,119 @@ auctionsRouter.post("/add", upload.single("image"), async (req, res) => {
       );
     }
 
+    if (!isHexAddress(owner)) {
+      throw new Error("Please provide a valid owner address");
+    }
+
     const ipfsFile = await ipfs.add(file.buffer);
 
-    const samePathAuctions = await Auction.find({
+    const samePathAuctions = await AuctionModel.find({
       path: ipfsFile.path,
     });
     if (samePathAuctions.length > 0) {
       throw new Error("This image has already been uploaded");
     }
 
-    const auction = await Auction.create({
+    const auction = await AuctionModel.create({
       title: title.trim(),
       startingPrice: parseFloat(startingPrice),
-      endingPrice: parseFloat(endingPrice),
+      endingAt: new Date(endingAt),
       category: category.trim(),
+      owner: owner.trim(),
       path: ipfsFile.path,
     });
 
     res.send({
       auction,
+    });
+  } catch (error) {
+    res.status(400).send({
+      error,
+    });
+    Log.error(error);
+  }
+});
+
+auctionsRouter.get("/:id", async (req, res) => {
+  try {
+    const auction = await AuctionModel.findById(req.params.id).populate("bids");
+    if (!auction) {
+      throw new Error("Auction not found");
+    }
+
+    res.send({
+      auction,
+    });
+  } catch (error) {
+    res.status(400).send({
+      error,
+    });
+    Log.error(error);
+  }
+});
+
+auctionsRouter.get("/:id/bids", async (req, res) => {
+  try {
+    const auction = await AuctionModel.findById(req.params.id)
+      .select("bids")
+      .populate("bids");
+
+    if (!auction) {
+      throw new Error("Auction not found");
+    }
+
+    res.send({
+      bids: auction.bids,
+    });
+  } catch (error) {
+    res.status(400).send({
+      error,
+    });
+    Log.error(error);
+  }
+});
+
+auctionsRouter.post("/:id/bids/add", async (req, res) => {
+  try {
+    const { address, amount } = req.body;
+    const auction = await AuctionModel.findById(req.params.id).populate("bids");
+
+    if (!auction) {
+      throw new Error("Auction not found");
+    }
+
+    console.log(req.body);
+
+    if (!address || !amount) {
+      throw new Error("Please provide an address and amount");
+    }
+
+    if (
+      auction.bids.length > 0 &&
+      (parseFloat(amount) < auction.bids[0].amount ||
+        parseFloat(amount) < auction.startingPrice)
+    ) {
+      throw new Error(
+        "The bid cannot be lower than the current highest bid or the starting price"
+      );
+    }
+
+    if (!isHexAddress(address)) {
+      throw new Error("Please provide a valid address");
+    }
+
+    const bid = await AuctionBidModel.create({
+      address: address.trim(),
+      amount: parseFloat(amount),
+    });
+
+    await AuctionModel.updateOne(
+      { _id: req.params.id },
+      { $push: { bids: bid } }
+    );
+
+    res.send({
+      bid,
     });
   } catch (error) {
     res.status(400).send({
